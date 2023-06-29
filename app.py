5bf404c1a9bb78e6376d26a85aecd3b555f7d32d
ProgrammingGeekDeveloper <kingwriter2021@gmail.com>
2023-06-29 16:10:47 +0800

    'version3'

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-secret_word = 'giraffe'
-guess = ''
-guess_count = 0
-guess_limit = 3
-out_of_guesses = False
-
-while guess != secret_word and not out_of_guesses:
-    if guess_count < guess_limit:
-        guess = input('Enter guess: ')
-        guess_count += 1
-    else:
-        out_of_guesses = True
-
-if out_of_guesses:
-    print('out of guesses, You Lose!')
-else:
-    print('You win!')
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [4, 5, 6],
+    [0]
+]
+print(number_grid[0][0])
 
 
 
